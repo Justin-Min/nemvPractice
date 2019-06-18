698b7a868196d7032634ee230a2cfdcae16e2413
Justin-Min <sa3333@naver.com>
2019-06-18 22:24:18 +0900

    NEMV 기본편 완료

--- be/routes/api/user/index.js
+++ be/routes/api/user/index.js
@@ -1,41 +1,58 @@
 var express = require('express');
 var createError = require('http-errors');
 var router = express.Router();
-
-const us = [
-  {
-    name: 'Justin',
-    age: 14
-  },
-  {
-    name: '민인혁',
-    age: 39
-  }
-]
+const User = require('../../../models/users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(req.query)
-  console.log(req.body)
-  res.send({users:us})
-});
+  User.find()
+    .then(r => {
+      res.send({ success: true, users: r })
+    })
+    .catch(e => {
+      res.send({ success: false })
+    })
+})
+
 router.post('/', (req, res, next) => {
-  console.log(req.query)
-  console.log(req.body)
-  res.send({success:true, msg:'post ok'});
-});
-router.put('/', (req, res, next) => {
-  console.log(req.query)
-  console.log(req.body)
-  res.send({success:true, msg:'put ok'});
-});
-router.delete('/', (req, res, next) => {
-  console.log(req.query)
-  console.log(req.body)
+  const { name, age } = req.body
+  const u = new User({ name, age })
+  u.save()
+    .then(r => {
+      res.send({ success: true, msg: r })
+    })
+    .catch(e => {
+      res.send({ success: false, msg: e.message })
+    })
+})
+
+router.put('/:id', (req, res, next) => {
+  const id = req.params.id
+  const { name, age } = req.body
+  User.updateOne({ _id: id }, { $set: { name, age }})
+  .then(r => {
+    res.send({ success: true, msg: r })
+  })
+  .catch(e => {
+    res.send({ success: false, msg: e.message })
+  })
+  // res.send({success:true, msg:'put ok'});
+})
+
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id
+  User.deleteOne({ _id: id })
+  .then(r => {
+    res.send({ success: true, msg: r })
+  })
+  .catch(e => {
+    res.send({ success: false, msg: e.message })
+  })
   res.send({success:true, msg:'delete ok'});
-});
+})
+
 router.all('*', function(req, res, next) {
   next(createError(404, 'There is no shch API!'));
-});
+})
 
 module.exports = router;
--- fe/src/views/User.vue
+++ fe/src/views/User.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container grid-list-md text-xs-center>
     <v-layout row wrap>
-      <v-flex xs12 sm3>
+      <!-- <v-flex xs12 sm3>
         <v-card>
           <v-card-title primary-title>
             <div>
@@ -64,8 +64,84 @@
             <v-btn flat color="orange" @click="delReq">Submit</v-btn>
           </v-card-actions>
         </v-card>
+      </v-flex> -->
+      <v-flex xs12 sm6 md4 v-for="user in users" :key="user._id">
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">{{ user.name }}</h3>
+              <div> {{ user.age }} </div>
+            </div>
+          </v-card-title>
+
+          <v-card-actions>
+            <v-btn flat color="orange" @click="putDialog(user)">수정</v-btn>
+            <v-btn flat color="error" @click="delUser(user._id)">삭제</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
+    <v-btn
+      absolute
+      dark
+      fab
+      bottom
+      right
+      color="green"
+      @click=mdUp
+    ><v-icon>add</v-icon>
+    </v-btn>
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <!-- <template v-slot:activator="{ on }">
+        <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
+      </template> -->
+      <v-card>
+        <v-card-title>
+          <span class="headline">User Profile</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12 sm6 md4>
+                <v-text-field
+                  label="Legal last name*"
+                  hint="example of persistent helper text"
+                  persistent-hint
+                  required
+                  v-model="userName"
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-select
+                  :items="userAges"
+                  label="Age*"
+                  required
+                  v-model="userAge"
+                ></v-select>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click="putUser">수정</v-btn>
+          <v-btn color="blue darken-1" flat @click="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" flat @click="postUser">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-snackbar
+      v-model="snackbar"
+    >
+      {{ sbMsg }}
+      <v-btn
+        color="pink"
+        flat
+        @click="snackbar = false"
+      >
+        Close
+      </v-btn>
+    </v-snackbar>
   </v-container>
 </template>
 
@@ -83,10 +159,21 @@
         getMd: '',
         postMd: '',
         putMd: '',
-        delMd: ''
+        delMd: '',
+        dialog: false,
+        userAges: [],
+        userAge: 0,
+        userName: '',
+        snackbar: false,
+        sbMsg: '',
+        putId: ''
       }
     },
     mounted () {
+      for (let i = 10; i < 40; i++) {
+        this.userAges.push(i)
+      }
+      this.getUsers()
     },
     methods: {
       getReq () {
@@ -102,7 +189,7 @@
       },
       postReq () {
         axios.post('http://localhost:3000/api/user', {
-          user: 'postMan'
+          name: '가정', age: 444
         })
         .then((r) => {
           this.postMd = JSON.stringify(r.data)
@@ -132,6 +219,70 @@
         .catch((e) => {
           console.log(e.message)
         })
+      },
+      mdUp () {
+        this.userName = ''
+        this.userAge = ''
+        this.dialog = true
+      },
+      postUser () {
+        // console.log(this.userName, this.userAge)
+        this.dialog = false
+        axios.post('http://localhost:3000/api/user', {
+          name: this.userName,
+          age: this.userAge
+        })
+        .then((r) => {
+          this.pop('사용자 등록 완료')
+          this.getUsers()
+        })
+        .catch((e) => {
+          // console.log(e.message)
+          this.pop(e.message)
+        })
+      },
+      getUsers() {
+        axios.get('http://localhost:3000/api/user')
+        .then((r) => {
+          console.log(r.data)
+          this.users = r.data.users
+        })
+        .catch((e) => {
+          this.pop(e.message)
+        })
+      },
+      putDialog(user) {
+        this.putId = user._id
+        this.dialog = true
+        this.userName = user.name
+        this.userAge = user.age
+      },
+      putUser() {
+        this.dialog = false
+        axios.put(`http://localhost:3000/api/user/${this.putId}`, {
+          name: this.userName, age: this.userAge
+        })
+        .then((r) => {
+          this.pop('사용자 수정 완료')
+          this.getUsers()
+        })
+        .catch((e) => {
+          this.pop(e.message)
+        })
+      },
+      delUser(id) {
+        axios.delete(`http://localhost:3000/api/user/${id}`)
+        .then((r) => {
+          this.pop('사용자 삭제 완료')
+          this.getUsers()
+        })
+        .catch((e) => {
+          console.error(e.message)
+        })
+      },
+      pop (msg) {
+        this.snackbar = true
+        this.sbMsg = msg
       }
     }
   }
